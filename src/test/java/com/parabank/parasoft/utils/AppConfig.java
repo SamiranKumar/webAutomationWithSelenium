7b4f4805f51e27d069c393ec93a6b561108cb13a
SamiranDevo <samirankumar11@gmail.com>
2023-08-09 01:47:07 +0600

    update

--- src/test/java/com/parabank/parasoft/utils/AppConfig.java
+++ src/test/java/com/parabank/parasoft/utils/AppConfig.java
@@ -3,6 +3,9 @@ package com.parabank.parasoft.utils;
 import java.time.Duration;
 
 public class AppConfig {
+    public final static String PROJECT_ABSOLUTE_PATH = "/webAutomationWithSelenium";
+
     public final static Duration WEB_DRIVER_WAIT_DURATION = Duration.ofSeconds(30);
+
 
 }
